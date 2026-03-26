7bf1600be11c7558b9160438d3af2141dd4c8d64
Shuu <shuu.n@protonmail.com>
2026-03-26 16:26:55 +0900

    Add SCRAM server-first field validation (#102)

--- tests/test_largeobject.nim
+++ tests/test_largeobject.nim
@@ -1,6 +1,6 @@
 import std/[unittest, importutils]
 
-import ../async_postgres/[async_backend, pg_client, pg_types, pg_largeobject]
+import ../async_postgres/[async_backend, pg_client, pg_largeobject]
 import ../async_postgres/pg_connection {.all.}
 
 privateAccess(PgConnection)
