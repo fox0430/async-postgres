00ae254da1368a2b288c2bb58b549955daf897cc
Shuu <shuu.n@protonmail.com>
2026-04-01 18:30:15 +0900

    Rename: closeCursor -> close (#159)

--- async_postgres/pg_client.nim
+++ async_postgres/pg_client.nim
@@ -2497,7 +2497,7 @@ proc closeCursorImpl(
 
   cursor.exhausted = true
 
-proc closeCursor*(cursor: Cursor): Future[void] {.async.} =
+proc close*(cursor: Cursor): Future[void] {.async.} =
   ## Close the cursor and return the connection to ready state.
   ## On timeout, the connection is marked csClosed (protocol out of sync).
   if cursor.timeout > ZeroDuration:
@@ -2524,7 +2524,7 @@ template withCursor*(
   try:
     body
   finally:
-    await cursorName.closeCursor()
+    await cursorName.close()
 
 proc openCursor*(
     conn: PgConnection,
