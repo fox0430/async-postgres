df80633f62298c510e784cbf4a216a5018fcf3b2
Shuu <shuu.n@protonmail.com>
2026-04-21 20:49:05 +0900

    Add cancel and abandonment E2E tests (#226)

--- tests/all_tests.nim
+++ tests/all_tests.nim
@@ -1,6 +1,7 @@
 {.push warning[UnusedImport]: off.}
 import
-  test_advisory_lock, test_auth, test_dsn, test_e2e, test_keepalive, test_largeobject,
-  test_network_failure, test_pool, test_protocol, test_protocol_fuzz, test_rowdata,
-  test_sql, test_ssl, test_tracing, test_types, test_pool_cluster
+  test_abandonment_e2e, test_advisory_lock, test_auth, test_cancel_e2e, test_dsn,
+  test_e2e, test_keepalive, test_largeobject, test_network_failure, test_pool,
+  test_protocol, test_protocol_fuzz, test_rowdata, test_sql, test_ssl, test_tracing,
+  test_types, test_pool_cluster
 {.pop.}
