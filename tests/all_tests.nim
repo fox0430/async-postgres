8e414b85e4fde62cc127dbcbbd3b14e7998634cd
Shuu <shuu.n@protonmail.com>
2026-05-29 18:43:13 +0900

    Add bounds checks to pgoutput decoder (#309)

--- tests/all_tests.nim
+++ tests/all_tests.nim
@@ -3,6 +3,6 @@ import
   test_abandonment_e2e, test_advisory_lock, test_auth, test_cancel_e2e, test_dsn,
   test_e2e, test_fill_recvbuf, test_keepalive, test_largeobject, test_network_failure,
   test_physical_replication, test_pool, test_protocol, test_protocol_fuzz,
-  test_replication_keepalive, test_rowdata, test_sql, test_ssl, test_tracing,
-  test_types, test_pool_cluster
+  test_replication, test_replication_keepalive, test_rowdata, test_sql, test_ssl,
+  test_tracing, test_types, test_pool_cluster
 {.pop.}
